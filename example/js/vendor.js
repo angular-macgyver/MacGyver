f730cac414f7b263be4df93e3704b2c8f4675615
Adrian Lee <adrianlee44@gmail.com>
2013-01-31 11:16:46 -0800

    Updated example.

--- example/js/vendor.js
+++ example/js/vendor.js
@@ -14601,5 +14601,4 @@ var styleDirective = valueFn({
   });
 
 })(window, document);
-angular.element(document).find('head').append('<style type="text/css">@charset "UTF-8";[ng\\:cloak],[ng-cloak],[data-ng-cloak],[x-ng-cloak],.ng-cloak,.x-ng-cloak{display:none;}ng\\:form{display:block;}</style>');;
-
+angular.element(document).find('head').append('<style type="text/css">@charset "UTF-8";[ng\\:cloak],[ng-cloak],[data-ng-cloak],[x-ng-cloak],.ng-cloak,.x-ng-cloak{display:none;}ng\\:form{display:block;}</style>');
