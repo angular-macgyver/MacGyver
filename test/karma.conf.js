5823b7b6a94e499e3010ee8ee762f9f848584fb3
Adrian Lee <adrianlee44@gmail.com>
2013-10-17 16:39:03 -0700

    test(karma.conf.js): Added angular-animate to karma

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -19,6 +19,7 @@ module.exports = function(config) {
       '../vendor/bower/jquery.ui/ui/jquery.ui.sortable.js',
       '../vendor/bower/jquery-file-upload/js/jquery.fileupload.js',
       '../vendor/bower/angular/angular.js',
+      '../vendor/bower/angular-animate/angular-animate.js',
 
       // Template //
       "template/*.html",
