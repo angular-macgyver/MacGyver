ef54cb48f95105b6ee5fa0e5b3cad7018dc9d042
Adrian Lee <adrianlee44@gmail.com>
2014-02-11 18:32:31 -0800

    test(protractor): Added e2e tests name

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -2,6 +2,11 @@ exports.config = {
   specs: ["e2e/*.spec.js"],
   baseUrl: "http://localhost:9001",
   framework: "jasmine",
+
+  capabilities: {
+    "name": "MacGyver E2E"
+  },
+
   onPrepare: function() {
     var disableNgAnimate;
     disableNgAnimate = function() {
