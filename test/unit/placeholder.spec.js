b859ec0601d1bff32ccba74cd1b637036387f5a5
Adrian Lee <adrianlee44@gmail.com>
2015-05-31 23:31:57 -0700

    test(macPlaceholder): Converted to Javascript

---
+++ test/unit/placeholder.spec.js
@@ -0,0 +1,33 @@
+describe("Mac placeholder", function() {
+  beforeEach(module("Mac"));
+
+  describe("initialization", function() {
+    var $compile, $rootScope;
+    beforeEach(inject(function(_$compile_, _$rootScope_) {
+      $compile = _$compile_;
+      $rootScope = _$rootScope_;
+    }));
+
+    it("should set placeholder with a scope variable", function() {
+      var element = $compile("<input mac-placeholder='placeholder' />")($rootScope);;
+      $rootScope.placeholder = "Test";
+      $rootScope.$digest();
+      
+      expect(element.prop("placeholder")).toBe("Test");
+    });
+
+    it("should set placeholder with string variable", function() {
+      var element = $compile("<input mac-placeholder='\"foobar\"' />")($rootScope);
+      $rootScope.$digest();
+
+      expect(element.prop("placeholder")).toBe("foobar");
+    });
+
+    it("should not have placeholder property", function() {
+      var element = $compile("<input mac-placeholder='placeholder' />")($rootScope);
+      $rootScope.$digest();
+
+      expect(element.prop("placeholder")).toBe("");
+    });
+  });
+});
