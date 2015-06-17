95287ace6d366b19ab6c015d1c640678678fae95
Adrian Lee <adrianlee44@gmail.com>
2015-06-17 00:08:23 -0700

    refactor(macTooltip): Convert to Javascript

---
+++ src/directives/tooltip.js
@@ -0,0 +1,163 @@
+/**
+ * @chalk overview
+ * @name Tooltip
+ *
+ * @description
+ * Tooltip directive
+ *
+ * @param {String}  mac-tooltip           Text to show in tooltip
+ * @param {String}  mac-tooltip-direction Direction of tooltip (default 'top')
+ * @param {String}  mac-tooltip-trigger   How tooltip is triggered (default 'hover')
+ * @param {Boolean} mac-tooltip-inside    Should the tooltip be appended inside element (default false)
+ * @param {Expr}    mac-tooltip-disabled  Disable and enable tooltip
+ */
+
+/**
+ * NOTE: This directive does not use $animate to append and remove DOM element or
+ * add and remove classes in order to optimize showing tooltips by eliminating
+ * the need for firing a $digest cycle.
+ */
+
+angular.module('Mac').directive('macTooltip', [
+  '$timeout',
+  'macTooltipDefaults',
+  'util',
+  function ($timeout, defaults, util) {
+    return {
+      restrict: 'A',
+      link: function ($scope, element, attrs) {
+        var tooltip, text, disabled, unobserve, closeDelay, opts;
+
+        opts = util.extendAttributes('macTooltip', defaults, attrs);
+
+        function showTip () {
+          var tip, offset, elementSize, tooltipSize, messageEl;
+
+          if (disabled || !text || tooltip) {
+            return true;
+          }
+
+          tip = opts.inside ? element : angular.element(document.body);
+
+          // Check if the tooltip still exists, remove if it does
+          removeTip(0);
+
+          messageEl = angular.element('<div class="tooltip-message"></div>');
+          messageEl.text(text);
+
+          tooltip = angular.element('<div />').addClass('mac-tooltip ' + opts.direction);
+          tooltip.append(messageEl);
+          
+          tip.append(tooltip);
+
+          // Only get element offset when not adding tooltip within the element
+          offset = opts.inside ? {top: 0, left: 0} : element.offset();
+
+          // Get element height and width
+          elementSize = {
+            width: element.outerWidth(),
+            height: element.outerHeight()
+          };
+
+          // Get tooltip width and height
+          tooltipSize = {
+            width: tooltip.outerWidth(),
+            height: tooltip.outerHeight()
+          };
+
+          // Adjust offset based on direction
+          switch (opts.direction) {
+            case 'bottom':
+            case 'top':
+              offset.left += elementSize.width / 2 - tooltipSize.width / 2;
+              break;
+            case 'left':
+            case 'right':
+              offset.top += elementSize.height / 2 - tooltipSize.height / 2;
+              break;
+          }
+
+          if (opts.direction == 'bottom') {
+            offset.top += elementSize.height;
+          } else if (opts.direction == 'top') {
+            offset.top -= tooltipSize.height;
+          } else if (opts.direction == 'left') {
+            offset.left -= tooltipSize.width;
+          } else if (opts.direction == 'right') {
+            offset.left += elementSize.width;
+          }
+
+          // Constrain the position
+          offset.top = Math.max(0, offset.top);
+          offset.left = Math.max(0, offset.left);
+
+          // Set the offset
+          angular.forEach(offset, function (value, key) {
+            if (!isNaN(+value) && angular.isNumber(+value)) {
+              value = value + "px";
+            }
+
+            tooltip.css(key, value);
+          });
+
+          tooltip.addClass(opts.class);
+          return true;
+        }
+
+        function removeTip (delay) {
+          delay = delay === undefined ? 100 : delay;
+
+          if (tooltip && !closeDelay) {
+            tooltip.removeClass(opts.class);
+
+            closeDelay = $timeout(function () {
+              if (tooltip) {
+                tooltip.remove();
+              }
+              tooltip = null;
+              closeDelay = null;
+            }, delay, false);
+          }
+
+          return true;
+        }
+
+        function toggle () { return tooltip ? removeTip() : showTip(); }
+
+        // Calling unobserve in the callback to simulate observeOnce
+        unobserve = attrs.$observe('macTooltip', function (value) {
+          if (value === undefined) {
+            return;
+          }
+
+          text = value;
+
+          if (opts.trigger !== 'hover' && opts.trigger !== 'click') {
+            throw new Error('macTooltip: Invalid trigger');
+          }
+
+          if (opts.trigger === 'click') {
+            element.bind('click', toggle);
+          } else if (opts.trigger === 'hover') {
+            element.bind('mouseenter', showTip);
+            element.bind('mouseleave click', function () {
+              removeTip();
+            });
+          }
+
+          unobserve();
+        });
+
+        if (attrs.macTooltipDisabled) {
+          $scope.$watch(attrs.macTooltipDisabled, function (value) {
+            disabled = !!value;
+          });
+        }
+
+        $scope.$on('$destroy', function () {
+          removeTip(0);
+        });
+      }
+    };
+  }
+]);
---
+++ test/unit/tooltip.spec.js
@@ -0,0 +1,154 @@
+describe("Mac Tooltip", function() {
+  var $compile, $rootScope, $timeout;
+
+  function queryTooltip () {
+    return document.querySelector(".mac-tooltip");
+  }
+
+  beforeEach(module("Mac"));
+  beforeEach(inject(function(_$compile_, _$rootScope_, _$timeout_) {
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+    $timeout = _$timeout_;
+  }));
+
+  afterEach(function() {
+    var i, tooltip, tooltips;
+    tooltips = document.querySelectorAll(".mac-tooltip");
+    for (i = 0; i < tooltips.length; i++) {
+      tooltip = tooltips[i];
+      tooltip.parentNode.removeChild(tooltip);
+    }
+  });
+
+  describe("Basic Initialization", function() {
+    it("should append to body on mouseenter", function() {
+      var tip = $compile("<div mac-tooltip='hello world'></div>")($rootScope);
+      $rootScope.$digest();
+
+      tip.triggerHandler("mouseenter");
+
+      expect(queryTooltip()).not.toBe(null);
+    });
+
+    it("should only append one tooltip", function() {
+      var tip, tooltip;
+      tip = $compile("<div mac-tooltip='hello world'></div>")($rootScope);
+      $rootScope.$digest();
+
+      tip.triggerHandler("mouseenter");
+      tip.triggerHandler("mouseenter");
+
+      tooltip = document.querySelectorAll(".mac-tooltip");
+      expect(tooltip.length).toBe(1);
+    });
+
+    it("should display the correct message", function() {
+      var text, tip = $compile("<div mac-tooltip='hello world'></div>")($rootScope);
+      $rootScope.$digest();
+
+      tip.triggerHandler("mouseenter");
+
+      text = queryTooltip().innerText || queryTooltip().textContent;
+      expect(text).toBe("hello world");
+    });
+  });
+
+  describe("Trigger", function() {
+    it("should remove tooltip on mouseleave", function() {
+      var tip = $compile("<div mac-tooltip='hello world'></div>")($rootScope);
+      $rootScope.$digest();
+
+      tip.triggerHandler("mouseenter");
+
+      expect(queryTooltip()).not.toBe(null);
+
+      tip.triggerHandler("mouseleave");
+      $timeout.flush();
+
+      expect(queryTooltip()).toBe(null);
+    });
+
+    it("should show and hide on click", function() {
+      var tip = $compile("<div mac-tooltip='test' mac-tooltip-trigger='click'></div>")($rootScope);
+      $rootScope.$digest();
+
+      tip.triggerHandler("click");
+
+      expect(queryTooltip()).not.toBe(null);
+
+      tip.triggerHandler("click");
+      $timeout.flush();
+
+      expect(queryTooltip()).toBe(null);
+    });
+
+    it("should throw an error with invalid trigger", function() {
+      var compile = function() {
+        var tip = $compile("<div mac-tooltip='test' mac-tooltip-trigger='trigger'></div>")($rootScope);
+        $rootScope.$digest();
+      };
+      expect(compile).toThrow();
+    });
+  });
+
+  describe("Direction", function() {
+    it("should set direction to top as default", function() {
+      var tip = $compile("<div mac-tooltip='test'></div>")($rootScope);
+      $rootScope.$digest();
+
+      tip.triggerHandler("mouseenter");
+
+      expect(queryTooltip().className.indexOf("top")).not.toBe(-1);
+    });
+
+    it("should set the direction to bottom", function() {
+      var tip = $compile("<div mac-tooltip='test' mac-tooltip-direction='bottom'></div>")($rootScope);
+      $rootScope.$digest();
+
+      tip.triggerHandler("mouseenter");
+
+      expect(queryTooltip().className.indexOf("bottom")).not.toBe(-1);
+    });
+  });
+
+  describe("disabled", function() {
+    it("should not create a tooltip", function() {
+      var tip = $compile("<div mac-tooltip='test' mac-tooltip-disabled='true'></div>")($rootScope);
+      $rootScope.$digest();
+
+      tip.triggerHandler("mouseenter");
+
+      expect(queryTooltip()).toBe(null);
+    });
+
+    it("should create a tooltip", function() {
+      var tip = $compile("<div mac-tooltip='test' mac-tooltip-disabled='false'></div>")($rootScope);
+      $rootScope.$digest();
+
+      tip.triggerHandler("mouseenter");
+
+      expect(queryTooltip()).not.toBe(null);
+    });
+  });
+
+  describe("Inside", function() {
+    it("should append tooltip inside of trigger", function() {
+      var tip = $compile("<div mac-tooltip='test' mac-tooltip-inside></div>")($rootScope);
+      $rootScope.$digest();
+
+      tip.triggerHandler("mouseenter");
+
+      expect(tip[0].querySelector(".mac-tooltip")).not.toBe(null);
+    });
+
+    it("should not append tooltip inside of trigger", function() {
+      var tip = $compile("<div mac-tooltip='test'></div>")($rootScope);
+      $rootScope.$digest();
+
+      tip.triggerHandler("mouseenter");
+
+      expect(tip[0].querySelector(".mac-tooltip")).toBe(null);
+    });
+  });
+});
