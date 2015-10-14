fa4bab82ffdf2f3f9afc3bedd7bce639b1f13104
Adrian Lee <adrianlee44@gmail.com>
2015-10-14 01:06:30 -0700

    refactor(macTagAutocomplete): Convert macTagAutocomplete to JS - Remove mac-tag-autocomplete-events - Remove mac-tag-autocomplete-url
    BREAKING CHANGES:
    - mac-tag-autocomplete-url has been removed

---
+++ src/directives/tag_autocomplete.js
@@ -0,0 +1,174 @@
+/**
+ * @chalk overview
+ * @name Tag Autocomplete
+ *
+ * @description
+ * A directive for generating tag input with autocomplete support on text input.
+ * Tag autocomplete has priority 800
+ *
+ * @dependencies
+ * - mac-autocomplete
+ * - mac-menu
+ *
+ * @param {String}  mac-tag-autocomplete-source      Data to use.
+ * Source support multiple types:
+ * - Array: An array can be used for local data and there are two supported formats:
+ *   - An array of strings: ["Item1", "Item2"]
+ *   - An array of objects with mac-autocomplete-label key: [{name:"Item1"}, {name:"Item2"}]
+ * - String: Using a string as the source is the same as passing the variable into mac-autocomplete-url
+ * - Function: A callback when querying for data. The callback receive two arguments:
+ *   - {String} Value currently in the text input
+ *   - {Function} A response callback which expects a single argument, data to user. The data will be
+ *   populated on the menu and the menu will adjust accordingly
+ * @param {String}  mac-tag-autocomplete-value       The value to be sent back upon selection (default "id")
+ * @param {String}  mac-tag-autocomplete-label       The label to display to the users (default "name")
+ * @param {Expr}    mac-tag-autocomplete-model       Model for autocomplete
+ * @param {Array}   mac-tag-autocomplete-selected    The list of elements selected by the user (required)
+ * @param {String}  mac-tag-autocomplete-query       The query parameter on GET command (defualt "q")
+ * @param {Integer} mac-tag-autocomplete-delay       Time delayed on fetching autocomplete data after keyup  (default 800)
+ * @param {String}  mac-tag-autocomplete-placeholder Placeholder text of the text input (default "")
+ * @param {Boolean} mac-tag-autocomplete-disabled    If autocomplete is enabled or disabled (default false)
+ * @param {Expr}    mac-tag-autocomplete-on-enter    When autocomplete is disabled, this function is called on enter, Should return either string, object or boolean. If false, item is not added
+ * - `item` - {String} User input
+ * @param {Event} mac-tag-autocomplete-clear-input $broadcast message; clears text input when received
+ */
+
+angular.module('Mac').directive('macTagAutocomplete', [
+  '$parse',
+  '$timeout',
+  'keys',
+  'util',
+  function ($parse, $timeout, keys, util) {
+    return {
+      restrict: 'E',
+      templateUrl: 'template/tag_autocomplete.html',
+      replace: true,
+      priority: 800,
+      scope: {
+        source: '=macTagAutocompleteSource',
+        placeholder: '=macTagAutocompletePlaceholder',
+        selected: '=macTagAutocompleteSelected',
+        disabled: '=macTagAutocompleteDIsabled',
+        model: '=macTagAutocompleteModel',
+        onEnter: '&macTagAutocompleteOnEnter',
+        onKeydown: '&macTagAutocompleteOnKeydown'
+      },
+
+      compile: function (element, attrs) {
+        var valueKey, labelKey;
+
+        valueKey = attrs.macTagAutocompleteValue || 'id';
+        labelKey = attrs.macTagAutocompleteLabel || 'name';
+
+        var valueGetter = $parse(valueKey);
+        var labelGetter = $parse(labelKey);
+
+        var queryKey = attrs.macTagAutocompleteQuery || 'q';
+
+        var delay = +attrs.macTagAutocompleteDelay
+        delay = isNaN(delay) ? 800 : delay;
+
+        var textInput = angular.element(element[0].getElementsByClassName('mac-autocomplete'));
+        textInput.attr({
+          'mac-autocomplete-label': labelKey,
+          'mac-autocomplete-query': queryKey,
+          'mac-autocomplete-delay': delay,
+          'mac-autocomplete-source': 'source'
+        });
+
+        return function ($scope, element, attrs) {
+          // Variable for input element
+          $scope.textInput = '';
+
+          // NOTE: Proxy is created to prevent tag autocomplete from breaking
+          // when user did not specify model
+          if (attrs.macTagAutocompleteModel) {
+            $scope.$watch('textInput', function (value) { $scope.model = value;});
+            $scope.$watch('model', function (value) { $scope.textInput = value;});
+          }
+
+          element.bind('click', function () {
+            var textInputDOM = element[0].querySelector('.mac-autocomplete');
+            textInputDOM.focus();
+          });
+
+          $scope.getTagLabel = function (tag) {
+            return labelKey ? labelGetter(tag) : tag;
+          };
+
+          // TODO (adrian): Look into removing this
+          updateAutocompleteSource = function () {
+            $scope.autocompletePlaceholder = $scope.selected && $scope.selected.length ? '' : $scope.placeholder;
+
+            if (!angular.isArray($scope.source) || !$scope.selected) {
+              $scope.autocompleteSource = $scope.source;
+              return;
+            }
+
+            $scope.autocompleteSource = $scope.source.filter(function (item) {
+              return $scope.selected.indexOf(item) > -1;
+            })
+          }
+
+          // NOTE: Watcher on source is added for string and function type to make sure
+          // scope value is copied correctly into this scope
+          if (angular.isArray($scope.source)) {
+            $scope.$watchCollection('source', updateAutocompleteSource);
+          } else {
+            $scope.$watch('source', updateAutocompleteSource);
+          }
+
+          $scope.onKeyDown = function ($event) {
+            var stroke = $event.which || $event.keyCode;
+            switch(stroke) {
+              case keys.BACKSPACE:
+                if (!$scope.textInput && angular.isArray($scope.selected)) {
+                  $scope.selected.pop();
+                }
+                break;
+              case keys.ENTER:
+                // Used when autocomplete is not needed
+                if ($scope.textInput && $scope.disabled) {
+                  $scope.onSelect($scope.textInput);
+                }
+                break;
+            }
+
+            $scope.onKeydown({
+              $event: $event,
+              value: $scope.textInput
+            });
+
+            return true;
+          };
+
+          $scope.onSuccess = function (data) {
+            // TODO (adrian): Filter out selected items
+            return data.data;
+          };
+
+          $scope.onSelect = function (item) {
+            var item;
+            if (attrs.macTagAutocompleteOnEnter) {
+              item = $scope.onEnter({item: item});
+            }
+
+            if (item && angular.isArray($scope.selected)) {
+              $scope.selected.push(item);
+            }
+
+            // NOTE: $timeout is added to allow user to access the model before
+            // clearing value in autocomplete
+            $timeout(function () {
+              $scope.textInput = ''
+            }, 0);
+          };
+
+          $scope.$on('mac-tag-autocomplete-clear-input', function () {
+            $scope.textInput = '';
+          });
+        };
+      }
+    }
+  }
+])
---
+++ test/unit/tag_autocomplete.spec.js
@@ -0,0 +1,283 @@
+describe("Mac tag autocomplete", function() {
+  var $compile, $rootScope, $timeout, hasClass, keys;
+
+  function hasClass(element, className) {
+    return element[0].className.indexOf(className) > -1;
+  };
+
+  beforeEach(module("Mac"));
+  beforeEach(module("template/tag_autocomplete.html"));
+  beforeEach(module("template/autocomplete.html"));
+  beforeEach(module("template/menu.html"));
+
+  beforeEach(inject(function(_$compile_, _$rootScope_, _$timeout_, _keys_) {
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+    $timeout = _$timeout_;
+    keys = _keys_;
+  }));
+
+  describe("basic initialization", function() {
+    it("should be replaced by the template", function() {
+      var element;
+      element = $compile("<mac-tag-autocomplete></mac-tag-autocomplete>")($rootScope);
+      $rootScope.$digest();
+      return expect(hasClass(element, "mac-tag-autocomplete")).toBeTruthy();
+    });
+
+    it("should set default value and label attribute on mac-autocomplete", function() {
+      var element, textInput;
+      element = $compile("<mac-tag-autocomplete></mac-tag-autocomplete>")($rootScope);
+
+      $rootScope.$digest();
+      textInput = element[0].querySelector(".mac-autocomplete");
+
+      expect(textInput.getAttribute("mac-autocomplete-label")).toBe("name");
+    });
+
+    it("should set value and label attribute on mac-autocomplete", function() {
+      var element, textInput;
+      element = $compile("<mac-tag-autocomplete mac-tag-autocomplete-label='test-name' mac-tag-autocomplete-value='test-id'></mac-tag-autocomplete>")($rootScope);
+
+      $rootScope.$digest();
+      textInput = element[0].querySelector(".mac-autocomplete");
+
+      expect(textInput.getAttribute("mac-autocomplete-label")).toBe("test-name");
+    });
+
+    it("should pass empty string as key and value", function() {
+      var element, textInput;
+      element = $compile("<mac-tag-autocomplete mac-tag-autocomplete-label='' mac-tag-autocomplete-value=''></mac-tag-autocomplete>")($rootScope);
+
+      $rootScope.$digest();
+      textInput = element[0].querySelector(".mac-autocomplete");
+
+      expect(textInput.getAttribute("mac-autocomplete-label")).toBe("");
+    });
+
+    it("should set query attribute on mac-autocomplete", function() {
+      var element, textInput;
+      element = $compile("<mac-tag-autocomplete mac-tag-autocomplete-query='query'></mac-tag-autocomplete>")($rootScope);
+
+      $rootScope.$digest();
+      textInput = element[0].querySelector(".mac-autocomplete");
+
+      expect(textInput.getAttribute("mac-autocomplete-query")).toBe("query");
+    });
+
+    it("should set delay attribute on mac-autocomplete", function() {
+      var element, textInput;
+      element = $compile("<mac-tag-autocomplete mac-tag-autocomplete-delay='100'></mac-tag-autocomplete>")($rootScope);
+
+      $rootScope.$digest();
+      textInput = element[0].querySelector(".mac-autocomplete");
+
+      expect(textInput.getAttribute("mac-autocomplete-delay")).toBe("100");
+    });
+
+    it("should set the source attribute on mac-autocomplete", function() {
+      var element, textInput;
+      $rootScope.source = ["test", "test1", "test2"];
+      element = $compile("<mac-tag-autocomplete mac-tag-autocomplete-source='source'></mac-tag-autocomplete>")($rootScope);
+
+      $rootScope.$digest();
+      textInput = element[0].querySelector(".mac-autocomplete");
+
+      expect(textInput.getAttribute("mac-autocomplete-source")).toBe("autocompleteSource");
+    });
+
+    it("should have the same source function as the parent scope", function() {
+      var element;
+      $rootScope.source = jasmine.createSpy("source");
+      element = $compile("<mac-tag-autocomplete mac-tag-autocomplete-source='source'></mac-tag-autocomplete>")($rootScope);
+
+      $rootScope.$digest();
+      $rootScope.$$childHead.autocompleteSource();
+
+      expect($rootScope.source).toHaveBeenCalled();
+    });
+
+    xit("should focus on autocomplete when click on tag autocomplete", function() {
+      var element, textInput;
+      $rootScope.source = ["test", "test1", "test2"];
+      element = $compile("<mac-tag-autocomplete mac-tag-autocomplete-source='source'></mac-tag-autocomplete>")($rootScope);
+
+      $rootScope.$digest();
+
+      element.triggerHandler("click");
+      textInput = element[0].querySelector(".mac-autocomplete");
+
+      expect(textInput).toBe(document.activeElement);
+    });
+
+    it("should model -> view", function() {
+      var element, textInput;
+      element = $compile("<mac-tag-autocomplete mac-tag-autocomplete-model='model'></mac-tag-autocomplete>")($rootScope);
+
+      $rootScope.$digest();
+      $rootScope.model = "Here";
+
+      $rootScope.$digest();
+      textInput = element[0].querySelector(".mac-autocomplete");
+
+      expect(angular.element(textInput).val()).toBe("Here");
+    });
+  });
+
+  describe("selected variable", function() {
+    it("should filter out used tags", function() {
+      var element, textInputScope;
+      $rootScope.source = [
+        {
+          id: "tag1"
+        }, {
+          id: "tag2"
+        }, {
+          id: "tag3"
+        }
+      ];
+      $rootScope.selected = [
+        {
+          id: "tag1"
+        }
+      ];
+      element = $compile("<mac-tag-autocomplete mac-tag-autocomplete-source='source' mac-tag-autocomplete-selected='selected'></mac-tag-autocomplete>")($rootScope);
+      $rootScope.$digest();
+
+      textInputScope = $rootScope.$$childHead;
+      expect(textInputScope.autocompleteSource.length).toBe(2);
+    });
+
+    it("should update autocompleteSource when source changes", function() {
+      var element, textInputScope;
+      $rootScope.source = [
+        {
+          id: "tag1"
+        }, {
+          id: "tag2"
+        }, {
+          id: "tag3"
+        }
+      ];
+      $rootScope.selected = [
+        {
+          id: "tag1"
+        }
+      ];
+
+      element = $compile("<mac-tag-autocomplete\n  mac-tag-autocomplete-source='source'\n  mac-tag-autocomplete-selected='selected'>\n</mac-tag-autocomplete>")($rootScope);
+
+      $rootScope.$digest();
+      textInputScope = $rootScope.$$childHead;
+
+      $rootScope.source.push({
+        id: "tag5"
+      });
+      $rootScope.$digest();
+
+      expect(textInputScope.autocompleteSource.length).toBe(3);
+    });
+
+    it("should have a placeholder", function() {
+      var element, textInputScope;
+      $rootScope.selected = [];
+      element = $compile("<mac-tag-autocomplete\n  mac-tag-autocomplete-placeholder = \"'Testing'\"\n  mac-tag-autocomplete-selected='selected'>\n</mac-tag-autocomplete>")($rootScope);
+
+      $rootScope.$digest();
+      textInputScope = $rootScope.$$childHead;
+
+      expect(textInputScope.autocompletePlaceholder).toBe("Testing");
+    });
+
+    it("should not have a placeholder when there is selcted tag", function() {
+      var element, textInputScope;
+      $rootScope.selected = [
+        {
+          id: "tag123"
+        }
+      ];
+      element = $compile("<mac-tag-autocomplete\n  mac-tag-autocomplete-placeholder = \"'Testing'\"\n  mac-tag-autocomplete-selected='selected'>\n</mac-tag-autocomplete>")($rootScope);
+      $rootScope.$digest();
+
+      textInputScope = $rootScope.$$childHead;
+
+      expect(textInputScope.autocompletePlaceholder).toBe("");
+    });
+  });
+
+  describe("onKeyDown", function() {
+    it("should fire keydown callback", function() {
+      var element, textInput;
+      $rootScope.keydown = jasmine.createSpy("keydown");
+      element = $compile("<mac-tag-autocomplete mac-tag-autocomplete-on-keydown='keydown()'></mac-tag-autocomplete>")($rootScope);
+
+      $rootScope.$digest();
+      textInput = element[0].querySelector(".mac-autocomplete");
+
+      browserTrigger(textInput, "keydown");
+
+      expect($rootScope.keydown).toHaveBeenCalled();
+    });
+
+    it("should remove the last tag", function() {
+      var element, textInput;
+      $rootScope.keydown = jasmine.createSpy('keydown');
+      $rootScope.selected = [
+        {
+          id: "tag1"
+        }
+      ];
+      element = $compile("<mac-tag-autocomplete mac-tag-autocomplete-selected='selected' mac-tag-autocomplete-on-keydown='keydown()'></mac-tag-autocomplete>")($rootScope);
+
+      $rootScope.$digest();
+      textInput = element[0].querySelector(".mac-autocomplete");
+
+      angular.element(textInput).triggerHandler({
+        type: "keydown",
+        which: keys.BACKSPACE
+      });
+
+      expect($rootScope.selected.length).toBe(0);
+    });
+
+    it("should push the text into selected", function() {
+      var element, textInput;
+      $rootScope.keydown = jasmine.createSpy('keydown')
+      $rootScope.selected = [];
+      element = $compile("<mac-tag-autocomplete mac-tag-autocomplete-selected='selected' mac-tag-autocomplete-on-keydown='keydown()' mac-tag-autocomplete-disabled='true'></mac-tag-autocomplete>")($rootScope);
+
+      $rootScope.$digest();
+      textInput = element[0].querySelector(".mac-autocomplete");
+
+      $rootScope.$$childHead.textInput = "Testing";
+      angular.element(textInput).triggerHandler({
+        type: "keydown",
+        which: keys.ENTER
+      });
+      $timeout.flush();
+
+      expect($rootScope.selected.length).toBe(1);
+    });
+  });
+
+  describe("on select callback", function() {
+    it("should fire callback", function() {
+      var element, textInput;
+      $rootScope.onEnter = jasmine.createSpy('onEnter')
+      $rootScope.selected = [];
+      element = $compile("<mac-tag-autocomplete mac-tag-autocomplete-selected='selected' mac-tag-autocomplete-on-enter='onEnter(item)' mac-tag-autocomplete-disabled='true'></mac-tag-autocomplete>")($rootScope);
+      $rootScope.$digest();
+
+      textInput = element[0].querySelector(".mac-autocomplete");
+      $rootScope.$$childHead.textInput = "Testing";
+
+      angular.element(textInput).triggerHandler({
+        type: "keydown",
+        which: keys.ENTER
+      });
+      $timeout.flush();
+
+      expect($rootScope.onEnter).toHaveBeenCalled();
+    });
+  });
+});
