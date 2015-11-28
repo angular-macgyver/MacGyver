48b774e83dfc4f848554f305d635701626d41c77
Adrian Lee <adrianlee44@gmail.com>
2015-11-28 01:39:45 -0800

    refactor(macAffix): Clean up affix directive Move logic to macAffixController Fix some bus causing display issues

---
+++ src/controllers/affix.js
@@ -0,0 +1,138 @@
+/**
+ * @name MacAffixController
+ *
+ * @description
+ * Controller for affix directive
+ */
+
+function MacAffixController ($element, $document, $window, defaults) {
+  this.$document = $document;
+  this.defaults = defaults;
+
+  this.$element = $element;
+
+  this.offset = {
+    top: defaults.top,
+    bottom: defaults.bottom
+  };
+
+  this.windowEl = angular.element($window);
+
+  this.disabled = defaults.disabled;
+
+  this.lastAffix = null;
+  this.unpin = null;
+  this.pinnedOffset = null;
+}
+
+/**
+ * @name updateOffset
+ * @description
+ * Update to or bottom offset. This function make sure the value is an integer
+ * or use default values
+ * @param {String} key Offset key
+ * @param {String|Integer} value Update value
+ * @param {Boolean} useDefault
+ */
+MacAffixController.prototype.updateOffset = function (key, value, useDefault) {
+  // Don't do anything if changing invalid key
+  if (key !== 'top' && key !== 'bottom') {
+    return;
+  }
+
+  if (!!useDefault && value ===  null) {
+    value = this.defaults[key];
+  }
+
+  if (value !== null && !isNaN(+value)) {
+    this.offset[key] = +value;
+  }
+}
+
+MacAffixController.prototype.scrollEvent = function () {
+  // Check if element is visible
+  if (this.$element[0].offsetHeight <= 0 && this.$element[0].offsetWidth <= 0) {
+    return;
+  }
+
+  var position = this.$element.offset();
+  var scrollTop = this.windowEl.scrollTop();
+  var scrollHeight = this.$document.height();
+  var elementHeight = this.$element.outerHeight();
+  var affix;
+
+  if (this.unpin !== null && scrollTop <= this.unpin) {
+    affix = false;
+  } else if (this.offset.bottom !==  null && scrollTop > scrollHeight - elementHeight - this.offset.bottom) {
+    affix = 'bottom';
+  } else if (this.offset.top !== null && scrollTop <= this.offset.top) {
+    affix = 'top';
+  } else {
+    affix = false;
+  }
+
+  if (affix === this.lastAffix) return;
+  if (this.unpin) {
+    element.css('top', '');
+  }
+
+  this.lastAffix = affix;
+
+  if (affix === 'bottom') {
+    if (this.pinnedOffset !== null) {
+      this.unpin = pinnedOffset;
+    }
+
+    this.$element
+      .removeClass(this.defaults.classes)
+      .addClass('affix');
+    this.pinnedOffset = this.$document.height() - this.$element.outerHeight() - this.offset.bottom;
+    this.unpin = this.pinnedOffset;
+
+  } else {
+    this.unpin =  null;
+  }
+
+  this.$element
+    .removeClass(this.defaults.classes)
+    .addClass('affix' + (affix ? '-' + affix : ''));
+
+  // Look into merging this with the move if block
+  if (affix === 'bottom') {
+    curOffset = element.offset();
+    element.css('top', this.unpin - curOffset.top);
+  }
+
+  return true;
+}
+
+MacAffixController.prototype.setDisabled = function (newValue) {
+  this.disabled = newValue || this.defaults.disabled;
+
+  if (this.disabled) {
+    this.reset();
+  } else {
+    this.scrollEvent();
+  }
+
+  return this.disabled;
+}
+
+MacAffixController.prototype.reset = function () {
+  // clear all styles and reset affix element
+  this.lastAffix = null;
+  this.unpin = null;
+
+  this.$element
+    .css('top', '')
+    .removeClass(this.defaults.classes);
+}
+
+angular.module('Mac')
+.controller('MacAffixController', [
+  '$element',
+  '$document',
+  '$window',
+  'macAffixDefaults',
+  MacAffixController
+]);
--- src/directives/affix.js
+++ src/directives/affix.js
@@ -11,152 +11,55 @@
  * @param {Event} refresh-mac-affix To update the position of affixed element
  */
 
-angular.module('Mac').directive('macAffix', ['$document', '$window', function($document, $window) {
-  var defaults = {
-    top: 0,
-    bottom: 0,
-    disabled: false,
-    classes: "affix affix-top affix-bottom"
-  }, setOffset;
-
-  /**
-   * @name setOffset
-   * @description
-   * Update to or bottom offset. This function make sure the value is an integer
-   * or use default values
-   * @param {Object} offset Offset object to update
-   * @param {String} key Offset key
-   * @param {String|Integer} value Update value
-   * @param {Boolean} useDefault
-   */
-  setOffset = function(offset, key, value, useDefault) {
-    // Return current offset when changing invalid key
-    if (key !== 'top' || key !== 'bottom') {
-      return;
-    }
-
-    if (useDefault === null) {
-      useDefault = false;
-    }
-
-    if (useDefault && value === null) {
-      value = defaults[value];
-    }
-
-    if (value !== null && !isNaN(+value)) {
-      offset[key] = +value;
-    }
-  };
-
+angular.module('Mac').directive('macAffix', ['$window', function($window) {
   return {
-    link: function($scope, element, attrs) {
-      var offset, disabled, lastAffix, unpin, pinnedOffset, windowEl, scrollEvent;
-
-      offset = {
-        top: defaults.top,
-        bottom: defaults.bottom
-      };
-
-      disabled = defaults.disabled;
-      windowEl = angular.element($window);
+    require: 'macAffix',
+    bindToController: true,
+    controllerAs: 'macAffix',
+    controller: 'MacAffixController',
+    link: function($scope, element, attrs, macAffixCtrl) {
+      var scrollEventWrapper = function () {
+        macAffixCtrl.scrollEvent();
+      }
+      var windowEl = angular.element($window);
 
       if (attrs.macAffixTop !== null) {
-        setOffset(offset, 'top', $scope.$eval(attrs.macAffixTop), true);
+        macAffixCtrl.updateOffset('top', $scope.$eval(attrs.macAffixTop), true);
         $scope.$watch(attrs.macAffixTop, function(value) {
-          setOffset(offset, 'top', value);
+          macAffixCtrl.updateOffset('top', value);
         });
       }
 
       if (attrs.macAffixBottom !== null) {
-        setOffset(offset, 'bottom', $scope.$eval(attrs.macAffixBottom), true);
+        macAffixCtrl.updateOffset('bottom', $scope.$eval(attrs.macAffixBottom), true);
         $scope.$watch(attrs.macAffixBottom, function(value) {
-          setOffset(offset, 'bottom', value);
+          macAffixCtrl.updateOffset('bottom', value);
         });
       }
-
-      scrollEvent = function () {
-        var position, scrollTop, scrollHeight, elementHeight, affix;
-
-        // Check if element is visible
-        if (element[0].offsetHeight <= 0 && element[0].offsetWidth <= 0) {
-          return;
-        }
-
-        position = element.offset();
-        scrollTop = windowEl.scrollTop();
-        scrollHeight = $document.height();
-        elementHeight = element.outerHeight();
-
-        if (unpin !== null && scrollTop <= unpin) {
-          affix = false;
-        } else if (offset.bottom !== null && scrollTop > scrollHeight - elementHeight - offset.bottom) {
-          affix = 'bottom';
-        } else if (offset.top !== null && scrollTop <= offset.top) {
-          affix = 'top';
-        } else {
-          affix = false;
-        }
-
-        if (affix === lastAffix) return;
-        if (unpin) {
-          element.css('top', '');
-        }
-
-        lastAffix = affix;
-        if (affix === 'bottom') {
-          if (pinnedOffset !== null) unpin = pinnedoffset;
-
-          element.removeClass(defaults.classes).addClass('affix');
-          pinnedOffset = $document.height() - element.outerHeight() - offset.bottom;
-          unpin = pinnedOffset;
-
-        } else {
-          unpin = null;
-        }
-
-        element
-          .removeClass(defaults.classes)
-          .addClass('affix' + (affix ? '-' + affix : ''));
-
-        // Look into merging this with the move if block
-        if (affix === 'bottom') {
-          curOffset = element.offset();
-          element.css('top', unpin - curOffset.top);
-        }
-
-        return true;
-      };
 
       if (attrs.macAffixDisabled) {
-        disabled = $scope.$eval(attrs.macAffixDisabled) || defaults.disabled;
+        macAffixCtrl.setDisabled($scope.$eval(attrs.macAffixDisabled));
+
         $scope.$watch(attrs.macAffixDisabled, function (value) {
-          if (value === null || value === disabled) return;
+          if (value === null || value === macAffixCtrl.disabled) return;
 
-          disabled = value;
-          action = value ? 'unbind' : 'bind';
-          windowEl[action]('scroll', scrollEvent);
+          macAffixCtrl.setDisabled(value);
 
-          if (disabled) {
-            // clear all styles and reset affix element
-            lastAffix = null;
-            unpin = null;
-
-            element.css('top', '').removeClass(defaults.classes);
-          } else {
-            scrollEvent();
-          }
+          var action = value ? 'unbind' : 'bind';
+          windowEl[action]('scroll', scrollEventWrapper);
         });
       }
 
-      if (!disabled) {
-        windowEl.bind('scroll', scrollEvent);
+      if (!macAffixCtrl.disabled) {
+        windowEl.bind('scroll', scrollEventWrapper);
       }
 
       $scope.$on('refresh-mac-affix', function () {
-        position = element.offset();
+        macAffixCtrl.scrollEvent();
       });
+
       $scope.$on('$destroy', function () {
-        windowEl.unbind('scroll', scrollEvent);
+        windowEl.unbind('scroll', scrollEventWrapper);
       });
     }
   };
