c385e70d3f2a26a961764d85104ae081cebb2371
Adrian Lee <adrianlee44@gmail.com>
2015-12-28 23:53:50 +0800

    test(macTime): Update macTime e2e tests

--- test/e2e/time.spec.js
+++ test/e2e/time.spec.js
@@ -2,9 +2,9 @@ describe("Mac Time Input", function() {
   var input, output;
   beforeEach(function() {
     browser.get("/test/e2e/time.html");
-    browser.waitForAngular();
-    input = element(By.css(".mac-date-time"));
-    output = element(By.css(".output"));
+
+    input = element(by.css(".mac-date-time"));
+    output = element(by.css(".output"));
   });
 
   afterEach(function() {
@@ -19,24 +19,33 @@ describe("Mac Time Input", function() {
 
   it("should change the meridian to AM after pressing the A button", function() {
     input.click();
+    browser.executeScript("return arguments[0].setSelectionRange(6, 8);", input.getWebElement());
+
     input.sendKeys("A");
+
     expect(output.getText()).toEqual("10:55 AM");
   });
 
-  xit("should not change the meridian to AM after pressing the P button", function() {
+  it("should not change the meridian to AM after pressing the P button", function() {
     input.click();
+    browser.executeScript("return arguments[0].setSelectionRange(6, 8);", input.getWebElement());
+
     input.sendKeys("P");
     expect(output.getText()).toEqual("10:55 PM");
   });
 
   it("should change the model after pressing down button", function() {
     input.click();
+    browser.executeScript("return arguments[0].setSelectionRange(6, 8);", input.getWebElement());
+
     input.sendKeys(protractor.Key.DOWN);
     expect(output.getText()).toEqual("10:55 AM");
   });
 
   it("should change the model after pressing up button", function() {
     input.click();
+    browser.executeScript("return arguments[0].setSelectionRange(6, 8);", input.getWebElement());
+
     input.sendKeys(protractor.Key.UP);
     expect(output.getText()).toEqual("10:55 AM");
   });
