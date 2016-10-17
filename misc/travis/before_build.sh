615375ddda4e259fe0ae2f04da0bf4b036a177e5
Adrian Lee <adrianlee44@gmail.com>
2016-10-16 20:35:42 -0700

    chore(travis): Stop installing bower when running tests on travis

--- misc/travis/before_build.sh
+++ misc/travis/before_build.sh
@@ -6,9 +6,7 @@ mkdir -p $LOGS_DIR
 
 ./misc/saucelabs/start_tunnel.sh
 
-npm install -g bower grunt-cli
-
-bower install
+npm install -g grunt-cli
 
 echo "wait_for_browser_provider"
 ./misc/travis/wait_for_browser_provider.sh
